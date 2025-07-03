eb75c0f2f781b0c0965447d85b04c45c1c1c9638
moyu <2447656632@qq.com>
2025-07-03 11:24:43 +0800

    处理config信息

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // 数据文件所在文件夹
-#define DATA_DIR "D://MyCodeCollection//CppCollection//studentSystem"
+#define DATA_DIR "D://###//###//###"
 
 // 学生信息的持久化文件
 #define DATA_FILE DATA_DIR "/student.txt"
